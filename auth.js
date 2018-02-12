81f6e5c2a0f84fddebd9f6cb07797b0544baadaf
marshall <prabhakarupadhyay1997@gmail.com>
2018-02-12 22:03:01 +0530

    adspotonline

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '28238042876-g9ogpu6l7ctpqg5rehe87mpbf5l7cahc.apps.googleusercontent.com',
         'clientSecret'  : 'NTLhss_3t3VmtHq4nuC1cGzT',
-        'callbackURL'   : 'http://nodejs-adseeker.rhcloud.com/auth/google/callback'
+        'callbackURL'   : 'http://adspotonline-adspotonline.a3c1.starter-us-west-1.openshiftapps.com/auth/google/callback'
     }
 
 };
